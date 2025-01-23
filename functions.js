4b4f24bc568d3e25f0e0acb98fb60dd0ed32a587
lolpowerluke <47672899+lolpowerluke@users.noreply.github.com>
2025-01-23 13:45:01 +0000

    continued with job page and redesigned main page

--- functions.js
+++ functions.js
@@ -28,37 +28,43 @@ function drawMainPage() {
   context.arc(width/6*5-width/12, 100, width/10, 0, Math.PI*2);
   context.arc(width-width/12, 100, width/10, 0, Math.PI*2);
   context.fill();
+  drawPerson((width/36)/5*6, 3, width/36, "#2C463F");
+  drawPerson((width/36)/5*6, 2, width/36, "white");
+  drawPerson((width/36)/5*6, 1, width/36, "black");
+  drawPerson((width/36)/5*6*3, 2.2, width/36, "orange");
+  drawPerson((width/36)/5*6*3, 1.2, width/36, "#053447");
+  drawPerson((width/36)/5*6*5, 1.4, width/36, "red");
   context.beginPath();
-  context.fillStyle = "#DC1F26";
-  context.arc(width/6*5, width/18+100, width/24, 0, Math.PI*2);
+  context.fillStyle =  "white";
+  context.arc(width/10*9 + width/35, height/6*4-height/100, width/75, 0, Math.PI*2)
+  context.fill();
+  drawLines(width/10*9 + width/35, height/6*4-height/100, width/75, "black")
+}
+
+/**
+ * 
+ * @param {Number} x middle of top circle
+ * @param {Number} y middle of top circle
+ * @param {String} color color of the person
+ */
+function drawPerson(x, y, size, color) {
+  let width = context.canvas.width;
+  let height = context.canvas.height;
+
+  y = height - size*2*y
+
+  context.fillStyle = color;
+  context.shadowOffsetX = 0;
+  context.shadowOffsetY = 0;
+  context.shadowBlur = 0;
+  context.beginPath();
+  context.arc(x, y, size, -1.5*Math.PI,0.5*Math.PI);
   context.fill();
   context.beginPath();
-  context.fillStyle = "#053447";
-  context.arc(width/6-width/14, width/10+100, width/24, 0, Math.PI*2);
+  context.arc(x, y+size*2, size/5*6, Math.PI, 0);
+  context.lineTo(x+size/5*6, y+size*4);
+  context.lineTo(x-size/5*6, y+size*4);
   context.fill();
-  context.beginPath();
-  context.fillStyle = "#2C463F";
-  context.arc(width/6+width/12, height-width/10, width/40, 0, Math.PI*2);
-  context.fill();
-  context.beginPath();
-  context.fillStyle = "#F0A71E";
-  context.arc(width/6-width/24, height, width/24, 0, Math.PI*2);
-  context.fill();
-  context.beginPath();
-  context.fillStyle = "#F0A71E";
-  context.arc(width-width/9, height/3*2, width/50, 0, Math.PI*2);
-  context.fill();
-  context.beginPath();
-  context.fillStyle = "#053447";
-  context.arc(width-width/12, height/2, width/30, 0, Math.PI*2);
-  context.fill();
-  drawLines(width-width/12, height/2, width/30, "white");
-  context.beginPath();
-  context.fillStyle = "#F9F8F3";
-  context.strokeStyle = "black"
-  context.arc(width/6*5-width/7, height-height/5, width/30, 0, Math.PI*2);
-  context.fill();
-  drawLines(width/6*5-width/7, height-height/5, width/30, "black");
 }
 
 function drawLines(center_x, center_y, radius, color) {
@@ -120,137 +126,137 @@ function drawJobs() {
   // drawJobsCanvas2();
 }
 
-function drawJobsCanvas1() {
-  canvas = document.getElementById("canvas1");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  context = canvas.getContext("2d");
+// function drawJobsCanvas1() {
+//   canvas = document.getElementById("canvas1");
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+//   context = canvas.getContext("2d");
 
-  let width = context.canvas.width;
-  let height = context.canvas.height;
+//   let width = context.canvas.width;
+//   let height = context.canvas.height;
 
-  let pointsArray = [
-    {
-      p1: {
-        x: 0,
-        y: height/6*3
-      },
-      center: {
-        x: width/12,
-        y: height/6*4-height/12
-      },
-      p2: {
-        x: width/6,
-        y: height/6*4-height/12
-      }
-    },
-    {
-      p1: {
-        x: 0,
-        y: 0
-      },
-      center: {
-        x: width/6*2-width/18,
-        y: height/6*5
-      },
-      p2: {
-        x: width/6,
-        y: height/6*5
-      }
-    },
-    {
-      p1: {
-        x: 0,
-        y: 0
-      },
-      center: {
-        x: width/6*3+width/12,
-        y: height/6*7
-      },
-      p2: {
-        x: width/6,
-        y: height/6*4.8
-      }
-    },
-    {
-      p1: {
-        x: 0,
-        y: 0
-      },
-      center: {
-        x: width/6*5+width/12,
-        y: height/6*5
-      },
-      p2: {
-        x: width,
-        y: height/6*4.8
-      }
-    },
-    {
-      p1: {
-        x: 0,
-        y: 0
-      },
-      center: {
-        x: 0,
-        y: 0
-      },
-      p2: {
-        x: 0,
-        y: 0
-      }
-    },
-  ]
+//   let pointsArray = [
+//     {
+//       p1: {
+//         x: 0,
+//         y: height/6*3
+//       },
+//       center: {
+//         x: width/12,
+//         y: height/6*4-height/12
+//       },
+//       p2: {
+//         x: width/6,
+//         y: height/6*4-height/12
+//       }
+//     },
+//     {
+//       p1: {
+//         x: 0,
+//         y: 0
+//       },
+//       center: {
+//         x: width/6*2-width/18,
+//         y: height/6*5
+//       },
+//       p2: {
+//         x: width/6,
+//         y: height/6*5
+//       }
+//     },
+//     {
+//       p1: {
+//         x: 0,
+//         y: 0
+//       },
+//       center: {
+//         x: width/6*3+width/12,
+//         y: height/6*7
+//       },
+//       p2: {
+//         x: width/6,
+//         y: height/6*4.8
+//       }
+//     },
+//     {
+//       p1: {
+//         x: 0,
+//         y: 0
+//       },
+//       center: {
+//         x: width/6*5+width/12,
+//         y: height/6*5
+//       },
+//       p2: {
+//         x: width,
+//         y: height/6*4.8
+//       }
+//     },
+//     {
+//       p1: {
+//         x: 0,
+//         y: 0
+//       },
+//       center: {
+//         x: 0,
+//         y: 0
+//       },
+//       p2: {
+//         x: 0,
+//         y: 0
+//       }
+//     },
+//   ]
 
-  for (let i = 0; i < pointsArray.length-1; i++) {
-    let diffX = pointsArray[i].p1.x - pointsArray[i].center.x;
-    let diffY = pointsArray[i].p1.y - pointsArray[i].center.y;
-    let radius = Math.abs(Math.sqrt(diffX*diffX + diffY*diffY));
-    let startAngle = Math.atan2(diffY, diffX);
-    let endAngle   = Math.atan2(pointsArray[i].p2.y - pointsArray[i].center.y, pointsArray[i].p2.x - pointsArray[i].center.x);
-    var x = pointsArray[i].center.x + radius * Math.cos(-((endAngle/Math.PI)*360)*Math.PI/180);
-    var y = pointsArray[i].center.y + radius * Math.sin(-((endAngle/Math.PI)*360)*Math.PI/180);
-    endAngle   = Math.atan2(y - pointsArray[i].center.y, x - pointsArray[i].center.x);
-    context = document.querySelector("canvas").getContext("2d");
+//   for (let i = 0; i < pointsArray.length-1; i++) {
+//     let diffX = pointsArray[i].p1.x - pointsArray[i].center.x;
+//     let diffY = pointsArray[i].p1.y - pointsArray[i].center.y;
+//     let radius = Math.abs(Math.sqrt(diffX*diffX + diffY*diffY));
+//     let startAngle = Math.atan2(diffY, diffX);
+//     let endAngle   = Math.atan2(pointsArray[i].p2.y - pointsArray[i].center.y, pointsArray[i].p2.x - pointsArray[i].center.x);
+//     var x = pointsArray[i].center.x + radius * Math.cos(-((endAngle/Math.PI)*360)*Math.PI/180);
+//     var y = pointsArray[i].center.y + radius * Math.sin(-((endAngle/Math.PI)*360)*Math.PI/180);
+//     endAngle   = Math.atan2(y - pointsArray[i].center.y, x - pointsArray[i].center.x);
+//     context = document.querySelector("canvas").getContext("2d");
 
-    pointsArray[i].p2.x = x;
-    pointsArray[i].p2.y = y;
-    pointsArray[i+1].p1.x = x;
-    pointsArray[i+1].p1.y = y;
-  }
+//     pointsArray[i].p2.x = x;
+//     pointsArray[i].p2.y = y;
+//     pointsArray[i+1].p1.x = x;
+//     pointsArray[i+1].p1.y = y;
+//   }
 
-  context.fillStyle = "#F9F8F3";
-  for (let j = 0; j < pointsArray.length-1; j++) {
-    let diffX = pointsArray[j].p1.x - pointsArray[j].center.x;
-    let diffY = pointsArray[j].p1.y - pointsArray[j].center.y;
-    let radius = Math.abs(Math.sqrt(diffX*diffX + diffY*diffY));
+//   context.fillStyle = "#F9F8F3";
+//   for (let j = 0; j < pointsArray.length-1; j++) {
+//     let diffX = pointsArray[j].p1.x - pointsArray[j].center.x;
+//     let diffY = pointsArray[j].p1.y - pointsArray[j].center.y;
+//     let radius = Math.abs(Math.sqrt(diffX*diffX + diffY*diffY));
 
-    context.beginPath();
-    context.arc(pointsArray[j].center.x, pointsArray[j].center.y, radius, 0, Math.PI*2, false);
-    context.rect(pointsArray[j].center.x-radius, pointsArray[j].center.y, radius*2, height)
-    context.fill();
-  }
+//     context.beginPath();
+//     context.arc(pointsArray[j].center.x, pointsArray[j].center.y, radius, 0, Math.PI*2, false);
+//     context.rect(pointsArray[j].center.x-radius, pointsArray[j].center.y, radius*2, height)
+//     context.fill();
+//   }
 
-  for (let i = 0; i < pointsArray.length-1; i++) {
-    let diffX = pointsArray[i].p1.x - pointsArray[i].center.x;
-    let diffY = pointsArray[i].p1.y - pointsArray[i].center.y;
-    let radius = Math.abs(Math.sqrt(diffX*diffX + diffY*diffY));
-    let startAngle = Math.atan2(diffY, diffX);
-    let endAngle   = Math.atan2(pointsArray[i].p2.y - pointsArray[i].center.y, pointsArray[i].p2.x - pointsArray[i].center.x);
+//   for (let i = 0; i < pointsArray.length-1; i++) {
+//     let diffX = pointsArray[i].p1.x - pointsArray[i].center.x;
+//     let diffY = pointsArray[i].p1.y - pointsArray[i].center.y;
+//     let radius = Math.abs(Math.sqrt(diffX*diffX + diffY*diffY));
+//     let startAngle = Math.atan2(diffY, diffX);
+//     let endAngle   = Math.atan2(pointsArray[i].p2.y - pointsArray[i].center.y, pointsArray[i].p2.x - pointsArray[i].center.x);
 
-    context.shadowOffsetX = 4;
-    context.shadowOffsetY = 2;
-    context.shadowBlur = 5;
-    context.shadowColor = "rgba(0,0,0,1)";
-    context.globalCompositeOperation = "source-atop";
+//     context.shadowOffsetX = 4;
+//     context.shadowOffsetY = 2;
+//     context.shadowBlur = 5;
+//     context.shadowColor = "rgba(0,0,0,1)";
+//     context.globalCompositeOperation = "source-atop";
 
-    context.beginPath();
-    context.strokeStyle = "rgb(220, 220, 220)";
-    context.arc(pointsArray[i].center.x, pointsArray[i].center.y, radius, startAngle, endAngle, false);
-    context.stroke();
+//     context.beginPath();
+//     context.strokeStyle = "rgb(220, 220, 220)";
+//     context.arc(pointsArray[i].center.x, pointsArray[i].center.y, radius, startAngle, endAngle, false);
+//     context.stroke();
 
-    context.shadowOffsetX = 0;
-    context.shadowOffsetY = 0;
-    context.shadowBlur = 0;
-  }
-}
+//     context.shadowOffsetX = 0;
+//     context.shadowOffsetY = 0;
+//     context.shadowBlur = 0;
+//   }
+// }
